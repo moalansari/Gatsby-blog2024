8935361bee622b64125dd275c1ba26046b56a6cb
Mohamed Al Ansari <mohammad.alansari@outlook.com>
2024-06-07 00:55:28 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Blog Site 2024`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
